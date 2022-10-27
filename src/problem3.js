2d6a612405c054ac89b71a301237c09e37b8cc64
mick243 <kjm3746@gmail.com>
2022-10-27 23:46:09 +0900

    문제 3번

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const readline = require("readline");
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+  const game365 = (end) => {
+      let clap = 0;
+      const trap365 = /3|6|9/;
+      for (let i = 1; i <= end; i++) {
+          if (trap365.test(i)) clap++;
+      }
+      return clap;
+  };
 }
-
 module.exports = problem3;
